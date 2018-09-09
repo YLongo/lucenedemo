04cc116582058769688558fbc3d06cc831ca899d
Volong <yhlona523@gmail.com>
2018-09-09 22:51:56 +0800

    lucene

--- src/github/io/volong/lucenedemo/chapter01/LuceneTest.java
+++ src/github/io/volong/lucenedemo/chapter01/LuceneTest.java
@@ -11,14 +11,19 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 
 public class LuceneTest {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
 		
 		/*
 		 *  索引文档 
@@ -35,7 +40,7 @@ public class LuceneTest {
 		
 		String text = "Lucene is an Information Retrieval library written in Java.";
 		
-		doc.add(new TextField("fieldName", text, Field.Store.YES));
+		doc.add(new TextField("Content", text, Field.Store.YES));
 		
 		indexWriter.addDocument(doc);
 		
@@ -47,6 +52,9 @@ public class LuceneTest {
 		indexWriter.deleteDocuments(new Term("id", "1"));
 		
 		
+		// 将会提交索引并关闭所有相关的文件以及释放 write.lock
+		indexWriter.close();
+		
 		/*
 		 * 查询文档
 		 */
@@ -54,7 +62,26 @@ public class LuceneTest {
 		
 		IndexSearcher indexSearcher = new IndexSearcher(reader);
 		
-		// 将会提交索引并关闭所有相关的文件以及释放 write.lock
-		indexWriter.close();
+		QueryParser parse = new QueryParser("Content", analyzer);
+		
+		Query query = parse.parse("Lucene");
+		
+		int hitsPerPage = 10;
+		
+		TopDocs docs = indexSearcher.search(query, hitsPerPage);
+		
+		ScoreDoc[] hits = docs.scoreDocs;
+		
+		int end = Math.min(docs.totalHits, hitsPerPage);
+		
+		System.out.println("Total Hits: " + docs.totalHits);
+		
+		System.out.println("Results: ");
+		
+		for (int i = 0; i < end; i++) {
+			Document d = indexSearcher.doc(hits[i].doc);
+			System.out.println("Content: " + d.get("Content"));
+		}
+		
 	}
 }
